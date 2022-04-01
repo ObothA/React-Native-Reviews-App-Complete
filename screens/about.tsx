9ec803cda88f87a44b152c14e77a1377d6c80904
obotha <oboth.abel.oa@gmail.com>
2022-04-01 11:17:56 +0300

    Add global styles to about and review details screens.

--- screens/about.tsx
+++ screens/about.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+
+import { globalStyles } from '../styles/global';
 
 export default () => {
   return (
     <View>
-      <Text style={styles.container}>About Screen</Text>
+      <Text style={globalStyles.container}>About Screen</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-});
--- screens/reviewDetails.tsx
+++ screens/reviewDetails.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+
+import { globalStyles } from '../styles/global';
 
 export default () => {
   return (
     <View>
-      <Text style={styles.container}>Review Details Screen</Text>
+      <Text style={globalStyles.container}>Review Details Screen</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-});
